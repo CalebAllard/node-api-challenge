b4f98c130d061000a66d5b3c51f19aa6b19daac0
CalebAllard <allard.caleb@gmail.com>
2020-01-17 13:48:10 -0500

    success connetion from react app to api

--- project-display/src/App.js
+++ project-display/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data,setData] = useState([]);
+  useEffect(() => {
+    axios('http://localhost:8000/api/projects')
+        .then(resp => {
+          console.log(resp); 
+          setData(resp.data);
+        })
+        .catch(err => {
+          console.log('axios err:',err)
+        })
+  
+  },[]);
+  
+  
+  
   return (
     <div className="App">
-      
+        {data.map(item =>
+          <div key={item.id}>
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+          </div>
+        )}
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 // const router = require()
 const projectsRouter = require('./routers/projects-router.js');
 const actionsRouter = require('./routers/actions-router.js');
 const {logger} = require('./middleware')
 //middleware
+server.use(cors());
 server.use(express.json());
 server.use(logger);
 //projects end points
